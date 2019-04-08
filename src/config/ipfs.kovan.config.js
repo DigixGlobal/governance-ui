9043e301f00882f8f519b942501c00bc5428f517
mrenoon <vunguyenvyvp@gmail.com>
2019-04-08 18:23:53 +0800

    update token configs

--- src/config/ipfs.kovan.config.js
+++ src/config/ipfs.kovan.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   GATEWAY: 'https://ipfs.digix.global/ipfs',
   ENDPOINT: 'https://ipfs-api.digix.global',
-  CONFIG: { host: 'ipfs.digix.global', protocol: 'https' },
+  CONFIG: { host: 'ipfs.infura.io', protocol: 'https' },
 };
